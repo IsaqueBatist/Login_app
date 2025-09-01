f68a4395db44c1fc5c93549d729223ce6012956d
Isaque Barbosa <isaquebatista400@gmail.com>
2025-09-01 09:03:09 -0300

    :sparkles: feat: Building AuthController

--- app/Controller/AuthController.php
+++ app/Controller/AuthController.php
@@ -3,4 +3,30 @@
 namespace App\Controller;
 use App\Service\AuthService;
 
+class AuthController{
+  private AuthService $service;
+  
+  public function _construct() {
+    $this->service = new AuthService();
+  }
+
+  public function handleLogin(): void {
+    $email = $_POST['email'] ?? '';
+    $password = $_POST['password'] ??  '';
+
+    if(empty($email) || empty($password)){
+      echo "Email e senha são obrigatórios";
+      return;
+    }
+
+    $isAuthenticated = $this->service->login($email, $password);
+
+    if($isAuthenticated){
+      echo "Login bem-sucedido";
+    }else{
+      echo "Credenciais inválidas";
+    }
+  }
+}
+
 ?>
