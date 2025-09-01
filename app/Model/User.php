1c970ef0ca34ec88a0bf15762a06f79a3137720d
Isaque Barbosa <isaquebatista400@gmail.com>
2025-09-01 10:01:40 -0300

    :sparkles: feat: added User class

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,3 +1,18 @@
 <?php
 //This is the user's class, making methods 
+namespace app\Model;
+
+class User {
+  public function __construct(
+    private int $id,
+    private string $email,
+    private string $password
+  ){}
+
+  public function getId(): int {return $this->id;}
+  public function getEmail(): string {return $this->email;}
+  public function getPassword(): string {return $this->password;}
+
+
+}
 ?>
