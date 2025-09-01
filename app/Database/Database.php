267b5b65bbb4ab6651b0ee6c19bae5f59609503a
Isaque Barbosa <isaquebatista400@gmail.com>
2025-09-01 09:54:29 -0300

    :sparkles: feat: creating database conecction methods

--- app/Database/Database.php
+++ app/Database/Database.php
@@ -1,5 +1,29 @@
 <?php
 // Connect with database
+namespace App\Database;
+use PDO;
+use PDOException;
+
+class Database {
+  private static ?PDO $connection = null;
+
+  public static function connect(): PDO{
+    if(self::$connection===null){
+      try {
+        self::$connection = new PDO(
+          'mysql:host=localhost;dbname=login_app;charset=utf8mb4',
+          'root',
+          ''
+        );
+        self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch (PDOException $e) {
+        die('Erro de conexão: ' . $e->getMessage());
+      }
+    }
+    return self::$connection;
+  }
+}
+
 
 
 ?>
